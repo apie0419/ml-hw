5d31d742f7c889242df1f9a8684950410ab9c9eb
APie <xu3mp6xjp6@gmail.com>
2020-05-05 03:27:36 +0800

    add desription

---
+++ hw2/README.md
@@ -0,0 +1,11 @@
+## Installation
+
+```
+conda create -f environment.yml -n <env_name>
+```
+
+## Config
+
+you need to modify parameters in config/config.yml
+
+desriptions for every parameters wrote in config/defaults.py
--- hw2/config/defaults.py
+++ hw2/config/defaults.py
@@ -10,7 +10,7 @@ _C.DEVICE = CN()
 _C.DATA = CN()
 
 _C.DEVICE.GPU = 1 # <gpu_id>
-_C.DEVICE.CUDA = True #<cuda_or_cpu>
+_C.DEVICE.CUDA = True # use gpu or not
 
 _C.PATH.TRAIN_SET = "" # <path_to_trainset>
 _C.PATH.TEST_SET = "" # <path_to_testset>
@@ -20,15 +20,15 @@ _C.MODEL.LR = 0.01 # <learning_rate>
 _C.MODEL.EPOCH = 0 # <train_epochs>
 
 # -----------------------------------------------
-# 標準化參數，也可以用其他的，這是推薦值
+# normalization parameters(suggestion)
 
 _C.DATA.PIXEL_MEAN = [0.485, 0.456, 0.406] 
 _C.DATA.PIXEL_STD = [0.229, 0.224, 0.225]
 
 # -----------------------------------------------
 
-_C.DATA.RESIZE = [224, 224] # RESIZE 圖片大小
-_C.DATA.NUM_WORKERS = 8 #用幾個 processors
+_C.DATA.RESIZE = [224, 224] # picture size after resizing
+_C.DATA.NUM_WORKERS = 8 # use how many processors
 _C.DATA.TRAIN_BATCH_SIZE = 32 # <train_batch_size>
 _C.DATA.TEST_BATCH_SIZE = 16 # <test_batch_size>
 
